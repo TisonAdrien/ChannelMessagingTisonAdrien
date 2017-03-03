ac131640367406b748028846d880073aaf17adf3
TisonAdrien <adrienbogalski@gmail.xom>
2017-03-03 16:39:38 +0100

    Design

--- app/src/main/java/adrien/tisonad/channelmessaging/LoginActivity.java
+++ app/src/main/java/adrien/tisonad/channelmessaging/LoginActivity.java
@@ -1,5 +1,6 @@
 package adrien.tisonad.channelmessaging;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -71,7 +73,7 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
                     editor.commit();
 
                     Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
-                    startActivity(myIntent);
+                    startActivity(myIntent, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, logoView, "logo").toBundle());
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Erreur de connexion", Toast.LENGTH_SHORT).show();
