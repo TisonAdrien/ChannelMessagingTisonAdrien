21b775189830190344c53b6d42eacd560e561b7f
TisonAdrien <adrienbogalski@gmail.xom>
2017-03-15 11:43:57 +0100

    Rework

--- app/src/main/java/adrien/tisonad/channelmessaging/LoginActivity.java
+++ app/src/main/java/adrien/tisonad/channelmessaging/LoginActivity.java
@@ -1,10 +1,12 @@
 package adrien.tisonad.channelmessaging;
 
+import android.animation.Animator;
 import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +15,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.dynamitechetan.flowinggradient.FlowingGradientClass;
 import com.google.gson.Gson;
 
 import java.io.BufferedInputStream;
@@ -35,6 +40,7 @@ import java.net.URLEncoder;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -47,9 +53,17 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
     private Button boutonValider;
     private ImageView logoView;
     public static final String PREFS_NAME = "PrefsFiles";
-    Handler mHandlerTada = new Handler(); // android.os.handler
+    public Handler mHandlerTada = new Handler(); // android.os.handler
+    public Handler mHandlerText = new Handler();
+    public Handler mHandlerTextFin = new Handler();
     int mShortDelay = 4000; //milliseconds
-
+    private static final String[] explainStringArray = {
+            "Connecte toi pour chatter avec tes amis",
+            "Blablabla",
+            "C'est moi Sancho le Cubain !",
+            "Voilà..."
+    };
+    private TextView textAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +74,14 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
         identifiant = (EditText) findViewById(R.id.Identifiant);
         password = (EditText) findViewById(R.id.Password);
         logoView = (ImageView) findViewById(R.id.logoView);
+        textAnim = (TextView) findViewById(R.id.textView4);
+
+        LinearLayout rl = (LinearLayout) findViewById(R.id.llBackground);
+        FlowingGradientClass grad = new FlowingGradientClass();
+        grad.setBackgroundResource(R.drawable.translate)
+                .onLinearLayout(rl)
+                .setTransitionDuration(4000)
+                .start();
 
         mHandlerTada.postDelayed(new Runnable(){
             public void run(){
@@ -69,6 +91,26 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
                 mHandlerTada.postDelayed(this, mShortDelay);
             }
         }, mShortDelay);
+
+
+        mHandlerText.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                YoYo.with(Techniques.SlideOutLeft).playOn(textAnim);
+                mHandlerTextFin.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        String txt = explainStringArray[new Random().nextInt(explainStringArray.length)];
+                        textAnim.setText(txt);
+                        YoYo.with(Techniques.SlideInRight).playOn(textAnim);
+                    }
+                },800);
+
+                mHandlerTada.postDelayed(this, 4000);
+            }
+        }, 4000);
+
+
     }
 
     @Override
@@ -88,7 +130,7 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
                 ConnectionReturn obj = gson.fromJson(content, ConnectionReturn.class);
 
                 if(obj.getResponse().equals("Ok")){
-                    Toast.makeText(getApplicationContext(), "Connecté", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(findViewById(R.id.buttonValider), "Connecté", Snackbar.LENGTH_LONG).show();
                     findViewById(R.id.textView4).clearAnimation();
                     SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
                     SharedPreferences.Editor editor = settings.edit();
@@ -103,13 +145,13 @@ public class LoginActivity extends AppCompatActivity implements OnDownloadComple
                     //startActivity(myIntent);
                 }
                 else{
-                    Toast.makeText(getApplicationContext(), "Erreur de connexion", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(findViewById(R.id.buttonValider), "Erreur de connection", Snackbar.LENGTH_LONG).show();
                     findViewById(R.id.textView4).clearAnimation();
                 }
             }
         });
         connexion.execute();
-        Toast.makeText(getApplicationContext(), "Connexion en cours...", Toast.LENGTH_SHORT).show();
+        Snackbar.make(findViewById(R.id.buttonValider), "Connection en cours...", Snackbar.LENGTH_LONG).show();
 
     }
 
