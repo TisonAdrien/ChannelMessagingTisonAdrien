196908f2b6ccb15fae3c5f48a2c31ed634e1fcaa
TisonAdrien <adrienbogalski@gmail.xom>
2017-03-22 16:53:19 +0100

    Version 22-03-2017

--- app/src/main/java/adrien/tisonad/channelmessaging/ChannelListActivity.java
+++ app/src/main/java/adrien/tisonad/channelmessaging/ChannelListActivity.java
@@ -2,14 +2,18 @@ package adrien.tisonad.channelmessaging;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.dynamitechetan.flowinggradient.FlowingGradientClass;
 import com.google.gson.Gson;
 
@@ -18,11 +22,15 @@ import java.util.HashMap;
 public class ChannelListActivity extends AppCompatActivity{
     private ListView channels;
     private Button btnFriends;
+    private ImageView logoView;
+    public Handler mHandlerLogo = new Handler();
+    int mShortDelay = 4000; //milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel_list);
+        logoView = (ImageView) findViewById(R.id.logoView);
 
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.rlBackground);
         FlowingGradientClass grad = new FlowingGradientClass();
@@ -30,6 +38,15 @@ public class ChannelListActivity extends AppCompatActivity{
                 .onRelativeLayout(rl)
                 .setTransitionDuration(4000)
                 .start();
+
+        mHandlerLogo.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.Pulse)
+                        .duration(1200)
+                        .playOn(logoView);
+                mHandlerLogo.postDelayed(this, mShortDelay);
+            }
+        }, mShortDelay);
 
         channels = (ListView) findViewById(R.id.listViewChannels);
         btnFriends = (Button) findViewById(R.id.buttonFriends);
